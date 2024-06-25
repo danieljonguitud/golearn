4cac795acfa495bf5d7498a486d9ca388e11c38b
danieljonguitud <daniel.jonguitud@sayvero.com>
2024-06-25 15:51:29 -0700

    added moving to use db and added getEvent endpoint

--- restapi/main.go
+++ restapi/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 	"danieljonguitud.com/restapi/db"
 	"danieljonguitud.com/restapi/models"
 	"github.com/gin-gonic/gin"
@@ -12,14 +13,38 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 
 	server.Run(":8080") // localhost:8080
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message":"Could not return results, please try again"})
+	}
 	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request"})
+		return
+	}
+
+	event, err := models.GetEvent(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not query DB"})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
+
+	return
 }
 
 func createEvent(context *gin.Context) {
@@ -31,8 +56,12 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
-	event.Id = 1
-	event.UserId = 1
-	event.Save()
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save into DB"})
+		return
+	}
+
 	context.JSON(http.StatusCreated, event)
 }
--- restapi/models/event.go
+++ restapi/models/event.go
@@ -1,22 +1,78 @@
 package models
 
-import "time"
+import (
+	"time"
+	"danieljonguitud.com/restapi/db"
+)
 
 type Event struct {
-	Id int `json:"id"`
-	Name string `json:"name"`
-	Description string `json:"description"`
-	Location string `json:"location"`
-	DateTime time.Time `json:"dateTime"`
+	Id int64 `json:"id"`
+	Name string `json:"name" binding:"required"`
+	Description string `json:"description" binding:"required"`
+	Location string `json:"location" binding:"required"`
+	DateTime time.Time `json:"dateTime" binding:"required"`
 	UserId int `json:"userId"`
 }
 
 var events = []Event{}
 
-func (e Event) Save() {
-	events = append(events, e)
+func (e *Event) Save() error {
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id)
+	VALUES (?, ?, ?, ?, ?)
+	`
+
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	result, err :=stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserId)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+
+	e.Id = id
+	
+	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		rows.Scan(&event.Id, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserId)
+		events = append(events, event)
+	}
+
+	return events, nil
+}
+
+func GetEvent(id int64) (*Event, error) {
+	query := "SELECT * FROM events WHERE id = ?"
+	row := db.DB.QueryRow(query, id)
+
+	var event Event
+	err := row.Scan(&event.Id, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserId)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &event, nil
 }
